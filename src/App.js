91d60f27da76fff34a920746e28c678fb5003d91
vnguyen1412 <Saikyouraikiri@gmail.com>
2023-07-06 13:12:17 -0400

    testing to see if it will commit to GitHub

--- src/App.js
+++ src/App.js
@@ -1,27 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
-import {Button, Box} from '@mui/material';
+import {Button, Box, Container} from '@mui/material';
 import {AspectRatio} from '@mui/joy';
+import backgroundPicture from './SAOWallpaper.jpg';
 import characterPicture from './Kirito.jpg';
 
 function CharacterPicture() {
   return(
     <Box
-      sx={{width: 300, height: 500, bgcolor: "blue"}}
+      sx={{width: 300, height: 500, bgcolor: "red"}}
       style={{position: "relative", top: "10px"}}
     >
-        <AspectRatio
-          variant="solid"
-          objectFit="contain"
-          ratio="3/4"
-          style={{position: "relative", top: "10px", left: "1px"}}
-        >
-          <img
-            src={characterPicture}
-            alt="Kirito"
-          />
-        </AspectRatio>
-      </Box>
+      <AspectRatio
+        variant="solid"
+        objectFit="contain"
+        ratio="3/4"
+        style={{position: "relative", top: "10px", left: "1px"}}
+      >
+        <img
+          src={characterPicture}
+          alt="Kirito"
+        />
+      </AspectRatio>
+    </Box>
   );
 }
 
@@ -35,22 +36,28 @@ function CharacterName() {
 
 function App() {
   return (
-    <>
+    <div
+      className="App-background"
+      style={{backgroundImage: `url(${backgroundPicture})` }}
+    >
       <div className="App-top">
-        <Box
+        {/* <CharacterPicture /> */}
+        {/* <Box
           sx={{width: 300, height: 500, bgcolor: "red"}}
           style={{position: "sticky", top: "500px", left: "300px"}}
         >
           <p>
             asdfasf
           </p>
-        </Box>
-        <Button>VT WAS HERE!</Button>
+        </Box> */}
+        <h1>
+          Sword Art Online Character Archive
+        </h1>
       </div>
       <div>
         <CharacterPicture />
       </div>
-    </>
+    </div>
   );
 }
 
